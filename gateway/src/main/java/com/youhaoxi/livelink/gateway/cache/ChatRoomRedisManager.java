6e7b7a6c6efed88d2c071707062963ab7886cd3a
panqiong <panqiong@youhaoxi.cn>
2018-05-18 14:34:12 +0800

    ++

--- gateway/src/main/java/com/youhaoxi/livelink/gateway/cache/ChatRoomRedisManager.java
+++ gateway/src/main/java/com/youhaoxi/livelink/gateway/cache/ChatRoomRedisManager.java
@@ -100,6 +100,7 @@ public class ChatRoomRedisManager {
      */
     public static void clearAllRelationThisHost(){
         Map<Integer, ChannelHandlerContext> map = ConnectionManager.getCtxMap();
+        logger.info("clearAllRelationThisHost: 有"+map.size()+" 个用户连接数据需要清理");
         if(map!=null){
             map.keySet().parallelStream().forEach(userId->{
                 clearUserIdRedisData( userId);
